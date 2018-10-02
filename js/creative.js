a280f8c4dd2c4910354ff8e5f649db7aea185458
maro0120 <37236059+maro0120@users.noreply.github.com>
2018-10-02 11:10:33 +0900

    Add files via upload

---
+++ js/creative.js
@@ -0,0 +1,153 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top )
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 57
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+  // Scroll reveal calls
+  window.sr = ScrollReveal();
+  sr.reveal('.sr-icons', {
+    duration: 600,
+    scale: 0.3,
+    distance: '0px'
+  }, 200);
+  sr.reveal('.sr-button', {
+    duration: 1000,
+    delay: 200
+  });
+  sr.reveal('.sr-contact', {
+    duration: 600,
+    scale: 0.3,
+    distance: '0px'
+  }, 300);
+
+  sr.reveal('.sr-left', {
+    origin: 'left',
+    duration: 3000,
+    scale: 1.0,
+    distance: '50px',
+    opacity: 0.5,
+    reset: 'true'
+  }, 300);
+
+  sr.reveal( '.sr-right', {
+    origin: 'right',
+    duration: 3000,
+    scale: 1.0,
+    distance: '500px',
+    opacity: 0.5,
+    reset: 'true'
+   });
+
+  sr.reveal( '.sr-bottom', {
+    origin: 'bottom',
+    duration: 2000,
+    scale: 1.0,
+    distance: '50px',
+    opacity: 0,
+    delay :200,
+    easing: 'cubic-bezier(.01, .52, .16, .98)'
+   });
+
+  sr.reveal( '.sr-bottom-300', {
+    origin: 'bottom',
+    duration: 1000,
+    scale: 1.0,
+    distance: '50px',
+    opacity: 0,
+    delay :300,
+    easing: 'cubic-bezier(.01, .52, .16, .98)'
+   });
+  sr.reveal( '.sr-bottom-400', {
+    origin: 'bottom',
+    duration: 1000,
+    scale: 1.0,
+    distance: '50px',
+    opacity: 0,
+    delay :400,
+    easing: 'cubic-bezier(.01, .52, .16, .98)'
+   });
+  sr.reveal( '.sr-bottom-500', {
+    origin: 'bottom',
+    duration: 1000,
+    scale: 1.0,
+    distance: '50px',
+    opacity: 0,
+    delay :500,
+    easing: 'cubic-bezier(.01, .52, .16, .98)'
+   });
+  sr.reveal( '.sr-bottom-600', {
+    origin: 'bottom',
+    duration: 1000,
+    scale: 1.0,
+    distance: '50px',
+    opacity: 0,
+    delay :600,
+    easing: 'cubic-bezier(.01, .52, .16, .98)'
+   });
+
+sr.reveal('.animate', { easing: 'ease', duration: 2000, distance: '400px', opacity: 1, scale: 1 ,reset: 'true'});
+
+            
+
+  // Magnific popup calls
+  $('.popup-gallery').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    tLoading: 'Loading image #%curr%...',
+    mainClass: 'mfp-img-mobile',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0, 1]
+    },
+    image: {
+      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+    }
+  });
+
+$('.test-popup-link').magnificPopup({ 
+  type: 'image',
+  gallery: {  //ギャラリーオプション
+    enabled:true
+  }
+
+});
+
+
+})(jQuery); // End of use strict
---
+++ js/index.js
@@ -0,0 +1,107 @@
+
+$(function () {
+    $('.fadein div:gt(0)').hide();
+    setInterval(function () {
+      $('.fadein :first-child').fadeOut("slow").next('div').fadeIn("slow").end().appendTo('.fadein');
+     }, 5000);
+  });
+  
+
+jQuery(function($) {
+    if (window.matchMedia('(max-width:480px)').matches) {
+    // SPの処理
+    $('.bg-slider').bgSwitcher({
+        images: ['img/001.jpg','img/002.jpg','img/003.jpg','img/004.jpg','img/005.jpg','img/006.jpg','img/007.jpg'],
+    });
+    // PCの処理
+    } else {
+    $('.bg-slider').bgSwitcher({
+        images: ['img/001.jpg','img/002.jpg','img/003.jpg','img/004.jpg','img/005.jpg','img/006.jpg','img/007.jpg'],
+    });
+  }
+});
+
+
+//読み込み最大記事数
+   var max_length = 15;
+   var max_news = 3;
+   var max_blog = 8;
+   var news_len = 0;
+   var blog_len = 0;
+function formatDt(dt_string) {
+    var dt = new Date(dt_string);
+    var y = dt.getFullYear();
+    var m = ('00' + (dt.getMonth() + 1)).slice(-2);
+    var d = ('00' + dt.getDate()).slice(-2);
+    return y + '.' + m + '.' + d;
+}
+  $(function(){
+    $.getJSON("https://query.yahooapis.com/v1/public/yql?callback=?", {
+      q: "select * from rss where url='http://www.salon-de-kukka.com/blog/?mode=rss'",
+      format: "json"
+    }, function(json) {
+      for (var i = 0; i < json.query.results.item.length; i++) {
+        if(i == max_length) break;
+
+        var entry = json.query.results.item[i];
+
+        var title = entry.title;//記事タイトル
+        var link = entry.link; //記事へのリンク
+        var fulldate = entry.pubDate; //投稿日
+        
+        date = formatDt(fulldate);
+            
+        //お知らせ用htmlを整形
+        if(title.indexOf("【お知らせ】") === 0){
+        console.log(title);
+          if(news_len < max_news){
+            news_len++;
+            var news_title = String(title).split('【お知らせ】').join('');
+            $('#feed').append('<dl><small><dt>' + date + '</dt></small><dd><a class="normal_color" href="' + link  +'" target="_blank">' + news_title +'</dd></dl>');
+            //$('#feed').append('<p><a href="' + link  +'" target="_blank">' + date + '　　　' + news_title + '</a></p>');
+            //$('#feed').append('<li><span class="date">'+date+'</span>'+ '　　　' + '<a href="' + link  +'" target="_blank">'+news_title + '</a></li>');
+          }
+          
+        //BLOGhtmlを整形
+        }else{
+            var reg = 'src="((?!https:\/\/stat100.ameba).)(.*?)(\.jpg|\.jpeg|\.gif|\.png|\/img_)(.*?)"';
+            if(entry.encoded) {
+                var imgsrc = entry.encoded.match(reg);
+            } else {
+                var imgsrc = entry.description.match(reg);
+            }
+            var dispImg = '';
+            if(imgsrc) {
+            var imgsrc = String(imgsrc).split('_m').join('');
+                var dispImg = '<img ' + imgsrc + ' class="img-fluid" alt="">';
+            }else{
+            var dispImg = '<img src="img/logo.png" class="img-fluid" alt="">';
+            }
+           //Start 出力したいHTMLに成形する部分 
+           var htmlstr = "";
+           htmlstr += '<div class="grid-item col-6 col-sm-6 col-lg-4 p-2"><a class="card hoverable card-image" href="' + entry.link + '" target="_blank">';
+           htmlstr += '<div class="view overlay hm-white-slight z-depth-1 flex-center">';
+           htmlstr += dispImg;
+           htmlstr += '<div class="mask flex-center rgba-black-light"><p class="white-text">' + date;
+           htmlstr += '<br>' + title;
+           htmlstr += '</p></div></div></a></div>';
+           
+           if(blog_len < max_blog){
+             blog_len++;
+             $('#feed_img').append(htmlstr);
+           }
+        }
+      }
+    var $gridindex = $('.grid-index');
+    $gridindex.imagesLoaded(function(){
+      $gridindex.masonry({
+                itemSelector: '.grid-item',
+                columnWidth: '.grid-sizer',
+                percentPosition: true
+      });
+      console.log("1");
+    });
+    });
+});
+
+
